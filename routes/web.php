fb063bc50ea2b724970ff66bc13151158ef255c1
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-07-15 22:00:28 +0700

    Introduction blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::view('contact', 'contact');
+Route::view('about', 'about');
+Route::view('login', 'login');
