4c4e09a39854a5f66d38d37c47e77fe1e111ab2d
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-07-17 20:39:12 +0700

    Add slug route when show the posts

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item {{ request()->is('/') ? 'active' : '' }}">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home</a>
       </li>
       <li class="nav-item {{ request()->is('about') ? 'active' : '' }}">
         <a class="nav-link" href="about">About</a>
