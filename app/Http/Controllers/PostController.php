52a5957eb62150fe2186468c278b185f6509fdcd
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-07-18 14:57:12 +0700

    Add post eloquent model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        $post = Post::where('slug', $slug)->firstOrFail();
         return view('posts.show', compact('post'));
     }
 }
