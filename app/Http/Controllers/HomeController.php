1528b62ddd436f7e62a63e9f57a10e7dd66ac524
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-07-27 19:07:59 +0700

    Add auth ui vue js

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Create a new controller instance.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
      */
-    public function __invoke()
+    public function __construct()
     {
-        $name = "Ivan";
-        return view('home', compact('name'));
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController');
-
-Route::get('posts', 'PostController@index');
-Route::get('posts/create', 'PostController@create');
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('posts/create', 'PostController@create')->name('posts.create');
 Route::post('posts/store', 'PostController@store');
 
 Route::get('posts/{post:slug}/edit', 'PostController@edit');
@@ -32,3 +30,6 @@ Route::get('posts/{post:slug}', 'PostController@show');
 Route::view('contact', 'contact');
 Route::view('about', 'about');
 Route::view('login', 'login');
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
